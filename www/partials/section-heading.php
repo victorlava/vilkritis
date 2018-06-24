57e58a458b2852997bcfed94ba57b4ad31f0b299
Victor Lava <hello@victorlava.com>
2018-06-24 11:46:23 +0300

    removed old code section-heading

--- www/partials/section-heading.php
+++ www/partials/section-heading.php
@@ -32,18 +32,4 @@
         </div>
     </div>
 
-
-    <!-- <div class="container">
-        <div class="row">
-            <div class="col-sm-8 col-md-6">
-                <h1>Procesas</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="section-page-background"></div>
-    <div class="section-page-background"></div> -->
-
 </section>
