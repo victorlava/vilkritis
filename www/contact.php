ef049f65843d6dfc473098d2f3415b6ff1483e71
Victor Lava <hello@victorlava.com>
2018-06-24 10:50:54 +0300

    updated smthing on contact page

--- www/contact.php
+++ www/contact.php
@@ -1,16 +1,32 @@
 <?php include('partials/head.php'); ?>
 
-<body id="contact">
+<body id="process">
     <!-- start of #home-page -->
 
-    <?php include('partials/navigation.php'); ?>
+    <?php include('partials/navigation-default.php'); ?>
 
     <main>
 
         <?php include('partials/section-heading-xs.php'); ?>
 
         <section class="section">
-
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <div class="content">
+                            <h2>Objekto Pavadinimas</h2>
+                            <p class="spaced">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="img img-line img-styling-left">
+                            <img src="/img/furniture.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <?php include('partials/quote.php'); ?>
