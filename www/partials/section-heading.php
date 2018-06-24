846fa1e86c41b7dd9e185401ad2f3f6751d58a9a
Victor Lava <hello@victorlava.com>
2018-06-24 11:45:56 +0300

    fixed process section-heading

--- www/partials/section-heading.php
+++ www/partials/section-heading.php
@@ -2,6 +2,39 @@
 
     <div class="container">
         <div class="row">
+            <div class="col-md-6">
+                <h1>Procesas</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="heading-flex">
+        <div class="heading-column background">
+
+        </div>
+
+        <div class="heading-column">
+
+        </div>
+
+        <div class="heading-column heading-reverse">
+            <div class="heading-row background">
+
+            </div>
+            <div class="heading-row">
+
+            </div>
+            <div class="heading-row background">
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- <div class="container">
+        <div class="row">
             <div class="col-sm-8 col-md-6">
                 <h1>Procesas</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -11,6 +44,6 @@
     </div>
 
     <div class="section-page-background"></div>
-    <div class="section-page-background"></div>
+    <div class="section-page-background"></div> -->
 
 </section>
