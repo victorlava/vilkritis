870fcdba1cef0508dbfe9b5d573614aef43c9b7a
Victor Lava <hello@victorlava.com>
2018-06-27 01:22:41 +0300

    added carousel

--- www/process.php
+++ www/process.php
@@ -14,7 +14,22 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-6">
                         <div class="img">
-                            <img src="/img/furniture.png" alt="">
+                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="/img/procesas-2.png" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="/img/procesas-2.png" alt="Second slide">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              </a>
+                            </div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-md-6 align-vertical">
