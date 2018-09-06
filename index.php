92e1bde700ae86e863fc916660ad6f95cbb1d5c9
Victor Lava <hello@victorlava.com>
2018-09-06 12:09:50 +0300

    added fonts, working on sizes, lineheights

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <main>
 
         <section class="section section-narrow">
-            <div class="section-image" style="background-image:url();">
+            <div class="section-image" style="background-image:url('img/rectangle.jpg');">
 
             </div>
             <div class="section-text">
@@ -24,7 +24,7 @@
               <h2>Apie mus</h2>
               <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
           </div>
-          <div class="jumbotron-media" style="background-image:url('');">
+          <div class="jumbotron-media" style="background-image:url('img/rectangle-7.jpg');">
 
           </div>
         </div>
@@ -39,9 +39,9 @@
                 <div class="row">
                     <div class="col-4">
                         <div class="card text-center">
-                          <img class="card-img-top rounded-circle" src="..." alt="Card image cap">
+                          <img class="card-img-top rounded-circle" src="img/oval-2.jpg" alt="Card image cap">
                           <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-link">Go somewhere</a>
                           </div>
@@ -49,9 +49,9 @@
                     </div>
                     <div class="col-4">
                         <div class="card text-center">
-                          <img class="card-img-top rounded-circle" src="..." alt="Card image cap">
+                          <img class="card-img-top rounded-circle" src="img/oval-2-copy.jpg" alt="Card image cap">
                           <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-link">Go somewhere</a>
                           </div>
@@ -59,9 +59,9 @@
                     </div>
                     <div class="col-4">
                         <div class="card text-center">
-                          <img class="card-img-top rounded-circle" src="..." alt="Card image cap">
+                          <img class="card-img-top rounded-circle" src="img/oval-2-copy-2.jpg" alt="Card image cap">
                           <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-link">Go somewhere</a>
                           </div>
--- partials/cta.php
+++ partials/cta.php
@@ -1,6 +1,6 @@
 <section class="section section-cta">
     <div class="container">
-        <h2>Pasiūlysime sprendimą jūsų įdėjai</h2>
+        <h2>Gaukite pasiūlymą</h2>
         <p class="text-white">The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling.</p>
         <a href="#" class="btn btn-light">Susisiekti</a>
     </div>
