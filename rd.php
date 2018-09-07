f33fa34019cc48688fbd4a9903b1e44bd0c302b0
Victor Lava <hello@victorlava.com>
2018-09-07 04:17:50 +0300

    added rd page, need to add parallax

---
+++ rd.php
@@ -0,0 +1,71 @@
+<?php include('partials/head.php'); ?>
+
+<body id="rd">
+    <!-- start of #home-page -->
+
+    <?php include('partials/navigation-white.php'); ?>
+
+    <main>
+
+        <section class="section section-wide">
+            <div class="section-image" style="background-image:url('img/rectangle-copy.jpg');">
+                <div class="shape shape-circle shape-circle-1"></div>
+            </div>
+            <div class="section-text">
+                <h1>
+                    R & D
+                </h1>
+                <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+            </div>
+        </section>
+
+        <section class="section section-gradient pr-md-3 pl-md-3">
+            <div class="container">
+                <div class="row mt-4">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="pl-1 pr-1">
+                            <h2 class="mt-1">Investicijos</h2>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="h-100 section-image section-image-shadow" style="background-image:url('img/rectangle-5.jpg');">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <div class="h-100 section-image section-image-shadow" style="background-image:url('img/rectangle-5.jpg');">
+
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="pl-1 pr-1">
+                            <h2 class="mt-1">Investicijos</h2>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="pl-1 pr-1">
+                            <h2 class="mt-1">Augantis spektras</h2>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="h-100 section-image section-image-shadow" style="background-image:url('img/rectangle-5.jpg');">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+<?php include('partials/footer.php') ?>
