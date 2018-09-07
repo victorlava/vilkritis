7b5dd9bed2138c3c8443abd08f0d961004b80f7a
Victor Lava <hello@victorlava.com>
2018-09-07 05:11:24 +0300

    started doing tiekimas page, tabs

---
+++ tiekimas.php
@@ -0,0 +1,106 @@
+<?php include('partials/head.php'); ?>
+
+<body id="tiekimas">
+    <!-- start of #home-page -->
+
+    <?php include('partials/navigation-white.php'); ?>
+
+    <main>
+
+        <section class="section section-wide">
+            <div class="section-image" style="background-image:url('img/rectangle-copy.jpg');">
+                <div class="shape shape-circle shape-circle-1"></div>
+            </div>
+            <div class="section-text">
+                <h1>
+                    Tiekimas
+                </h1>
+                <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+            </div>
+        </section>
+
+        <section class="section section-tabs section-gradient pr-md-3 pl-md-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Produktai</h2>
+                    </div>
+                </div>
+                <div class="section-tabs-wrapper">
+                    <div class="row">
+                        <div class="col-12">
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                              <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Koncentratai</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Priedai</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Žaliavos</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Užpildai</a>
+                              </li>
+                            </ul>
+                            <div class="tab-content" id="myTabContent">
+                              <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+                              <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                              <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Labai perdėtai ilgas produkto pavadinimas retiems atvejams</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Labai perdėtai ilgas produkto pavadinimas retiems atvejams</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Produkto pavadinimas</h5>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                <h5>Labai perdėtai ilgas produkto pavadinimas retiems atvejams</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+<?php include('partials/footer.php') ?>
