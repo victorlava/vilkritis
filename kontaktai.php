1d1570249af893d9948829aea4a058fc946a0021
Victor Lava <hello@victorlava.com>
2018-09-07 03:55:31 +0300

    kontaktai page almost done, need to add parallax shapes

---
+++ kontaktai.php
@@ -0,0 +1,115 @@
+<?php include('partials/head.php'); ?>
+
+<body id="kontaktai">
+    <!-- start of #home-page -->
+
+    <?php include('partials/navigation-white.php'); ?>
+
+    <main>
+
+        <section class="section section-wide">
+            <div class="section-image" style="background-image:url('img/rectangle-copy.jpg');">
+                <div class="shape shape-circle shape-circle-1"></div>
+            </div>
+            <div class="section-text">
+                <h1>
+                    Kontaktai
+                </h1>
+                <p>Granito g. 7, LT02241 Vilnius, Lietuva</p>
+                <p>Tel.: +370 5 2691102</p>
+                <p>Faks.: +370 5 2691103</p>
+                <p>El. paštas: info@vilkritis.com</p>
+                <p>PVM mokėtojo kodas: LT248525515</p>
+            </div>
+        </section>
+
+        <section class="section pt-2 pl-2 pr-2">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row mb-1">
+                            <div class="col-lg-7 col-xl-7">
+
+                            </div>
+                            <div class="col-lg-5 col-xl-5">
+                                <h2>Susisiekite su mumis</h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-7 col-xl-7">
+                                <div class="row mb-1">
+                                    <div class="col-12 col-sm-6 mb-1">
+                                        <div class="card card-contact">
+                                            <heading>
+                                                <h5>Vardenis Pavardenis</h5>
+                                                <p class="card-header-info">
+                                                    <span>Direktorius</span>
+                                                </p>
+                                            </heading>
+                                            <p>Tel.: +370 5 2691102</p>
+                                            <p>El. p.: info@vilkritis.com</p>
+                                            <a href="#" class="card-contact-link">
+                                                <img src="img/linkedin.png" width="32" alt="">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-6">
+                                        <div class="card card-contact">
+                                            <heading>
+                                                <h5>Vardenis Pavardenis</h5>
+                                                <p class="card-header-info">
+                                                    <span>Direktorius</span>
+                                                </p>
+                                            </heading>
+                                            <p>Tel.: +370 5 2691102</p>
+                                            <p>El. p.: info@vilkritis.com</p>
+                                            <a href="#" class="card-contact-link">
+                                                <img src="img/linkedin.png" width="32" alt="">
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-xl-5">
+                                <form>
+                                  <div class="row form-group">
+                                    <div class="col">
+                                      <input type="text" class="form-control" placeholder="Vardas">
+                                    </div>
+                                    <div class="col">
+                                      <input type="text" class="form-control" placeholder="El. paštas">
+                                    </div>
+                                  </div>
+                                  <div class="row form-group">
+                                    <div class="col">
+                                        <textarea class="form-control" rows="3" placeholder="Žinutė"></textarea>
+                                    </div>
+                                  </div>
+                                  <div class="row form-group">
+                                      <div class="col">
+                                          <button type="submit" class="btn btn-primary">Siųsti</button>
+                                      </div>
+                                  </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-1">
+                    <div class="col-12">
+                        <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2305.623228248468!2d25.225660451787157!3d54.69865758018789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd93c28fe5b441%3A0x338eed53cff1fabb!2sV.+Vaitkaus+g.+13%2C+Vilnius+04334!5e0!3m2!1sen!2slt!4v1536274662982" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <div class="map-info">
+                                <h4>Kaip mus rasti</h4>
+                                <p>Granito g. 7, LT02241</p>
+                                <p>Vilnius, Lietuva</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+<?php include('partials/footer.php') ?>
