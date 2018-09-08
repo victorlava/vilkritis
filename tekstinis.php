08bb43048ba29625b1454b05293ff50eb4e98068
Victor Lava <hello@victorlava.com>
2018-09-08 22:04:43 +0300

    added tekstinis page

---
+++ tekstinis.php
@@ -0,0 +1,30 @@
+<?php include('partials/head.php'); ?>
+
+<body id="tekstinis">
+    <!-- start of #home-page -->
+
+    <?php include('partials/navigation-white.php'); ?>
+
+    <main>
+        <section class="section section-text pt-5">
+
+                <h1>Tekstinis puslapis</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae proin sagittis nisl rhoncus mattis. Ipsum dolor sit amet consectetur adipiscing elit. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Aenean et tortor at risus. Netus et malesuada fames ac turpis egestas. Lectus nulla at volutpat diam ut venenatis tellus in metus. Aliquam sem et tortor consequat. Et netus et malesuada fames. Posuere morbi leo urna molestie at elementum eu facilisis sed. Sagittis eu volutpat odio facilisis mauris. Neque gravida in fermentum et sollicitudin ac orci. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Nisl rhoncus mattis rhoncus urna neque viverra justo nec.</p>
+                <p>Morbi tristique senectus et netus et malesuada fames ac. Donec ac odio tempor orci dapibus ultrices in. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Tempus iaculis urna id volutpat lacus. Sapien et ligula ullamcorper malesuada proin libero nunc consequat. Habitant morbi <a href="#">tristique</a> senectus et netus et. Suspendisse sed nisi lacus sed viverra tellus. A cras semper auctor neque vitae tempus quam pellentesque nec. Et sollicitudin ac orci phasellus egestas tellus rutrum. Varius morbi enim nunc faucibus a pellentesque sit amet. Eu turpis egestas pretium aenean pharetra magna ac placerat. Aliquet eget sit amet tellus cras adipiscing. Amet tellus cras adipiscing enim. Lacus viverra vitae congue eu. Nunc vel risus commodo viverra maecenas:</p>
+                <ul>
+                    <li>A cras semper auctor neque vitae tempus quam pellentesque nec. Et sollicitudin ac orci phasellus egestas tellus rutrum;</li>
+                    <li>Et netus et male</li>
+                    <li>Posuere morbi leo </li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <ul>
+                    <li>Lorem ipsum</li>
+                    <li>Et netus et male</li>
+                    <li>Posuere morbi leo </li>
+                </ul>
+
+        </section>
+
+    </main>
+
+<?php include('partials/footer.php') ?>
