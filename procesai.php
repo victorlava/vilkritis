d3919173b1e9f20e1b72dc7dbfd4d280becfa2c3
Victor Lava <hello@victorlava.com>
2018-09-09 11:53:40 +0300

    added procesai page

---
+++ procesai.php
@@ -0,0 +1,67 @@
+<?php include('partials/head.php'); ?>
+
+<body id="procesai">
+    <!-- start of #home-page -->
+
+    <?php include('partials/navigation-white.php'); ?>
+
+    <main>
+
+        <section class="section section-wide">
+            <div class="section-image" style="background-image:url('img/rectangle-copy.jpg');">
+                <div class="shape shape-circle shape-circle-1"></div>
+            </div>
+            <div class="section-text">
+                <h1>
+                    Procesai
+                </h1>
+                <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+            </div>
+        </section>
+
+        <section class="section section-gradient pl-3 pr-3">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 col-xxl-4">
+                        <div class="card card-process">
+                            <heading>
+                                <h3 class="card-title">Rotational</h3>
+                                <h3 class="card-subtitle">Molding</h3>
+                            </heading>
+                            <figure class="card-media" style="background-image:url('img/rectangle-11.jpg');">
+                                <figcaption>01</figcaption>
+                            </figure>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xxl-4">
+                        <div class="card card-process">
+                            <heading>
+                                <h3 class="card-title">Rotational</h3>
+                                <h3 class="card-subtitle">Molding</h3>
+                            </heading>
+                            <figure class="card-media" style="background-image:url('img/rectangle-11.jpg');">
+                                <figcaption>02</figcaption>
+                            </figure>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-xxl-4">
+                        <div class="card card-process">
+                            <heading>
+                                <h3 class="card-title">Rotational</h3>
+                                <h3 class="card-subtitle">Molding</h3>
+                            </heading>
+                            <figure class="card-media" style="background-image:url('img/rectangle-11.jpg');">
+                                <figcaption>03</figcaption>
+                            </figure>
+                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+<?php include('partials/footer.php') ?>
