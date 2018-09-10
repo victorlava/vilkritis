0a6556b1167ea7b0dd07ab5873500b8bba50a6e0
Victor Lava <hello@victorlava.com>
2018-09-10 11:27:29 +0300

    started doing accordion

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
         <section class="section section-shadow">
             <div class="container">
-                <div class="row">
+                <div class="row pb-2">
                     <div class="col-12 text-center">
                         <h2>Veikla</h2>
                     </div>
@@ -42,7 +42,7 @@
                 <div class="row">
                     <div class="col-12 col-sm-6 col-md-4">
                         <div class="card card-simple text-center">
-                          <img class="card-img-top rounded-circle" src="img/oval-2.jpg" alt="Card image cap">
+                          <img class="card-img-top rounded-circle pb-1" src="img/oval-2.jpg" alt="Card image cap">
                           <div class="card-body">
                             <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -52,7 +52,7 @@
                     </div>
                     <div class="col-12 col-sm-6 col-md-4">
                         <div class="card card-simple text-center">
-                          <img class="card-img-top rounded-circle" src="img/oval-2-copy.jpg" alt="Card image cap">
+                          <img class="card-img-top rounded-circle pb-1" src="img/oval-2-copy.jpg" alt="Card image cap">
                           <div class="card-body">
                             <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -62,13 +62,61 @@
                     </div>
                     <div class="col-12 col-sm-12 col-md-4">
                         <div class="card card-simple text-center">
-                          <img class="card-img-top rounded-circle" src="img/oval-2-copy.jpg" alt="Card image cap">
+                          <img class="card-img-top rounded-circle pb-1" src="img/oval-2-copy.jpg" alt="Card image cap">
                           <div class="card-body">
                             <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-link">Go somewhere</a>
                           </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section section-carousel">
+            <div class="container">
+                <div class="row text-center pb-2">
+                    <div class="col-12">
+                        <h2>Kodėl mus renkasi</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                      <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                      </ol>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img class="d-block w-100" src="img/rectangle-11.jpg" alt="First slide">
+                          <div class="carousel-text">
+                              <h4>Greitis ir lankstumas</h4>
+                              <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                          </div>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="img/rectangle-11.jpg" alt="Second slide">
+                          <div class="carousel-text">
+                              <h4>Greitis ir lankstumas</h4>
+                              <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="img/rectangle-11.jpg" alt="Third slide">
+                          <div class="carousel-text">
+                              <h4>Greitis ir lankstumas</h4>
+                              <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.</p>
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
                     </div>
                 </div>
             </div>
