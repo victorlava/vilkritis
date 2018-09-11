8344193afbbfc986b1ce13bb064f0c9df72fbf2c
Victor Lava <hello@victorlava.com>
2018-09-11 07:59:26 +0300

    modal animations

--- karjera.php
+++ karjera.php
@@ -44,42 +44,69 @@
                                   <div class="modal-dialog modal-lg " role="document">
                                     <div class="modal-content">
                                       <div class="modal-header">
-                                        <h4 class="modal-title" id="aplikuotiLabel1">Pozicijos pavadinimas</h4>
-                                        <p class="card-header-info">
-                                            <span>Vilnius</span>
-                                            <span>Galioja iki: 2018.07.01   </span>
-                                        </p>
+                                          <header class="modal-title">
+                                              <h4 id="aplikuotiLabel1">Pozicijos pavadinimas</h4>
+                                              <p class="card-header-info">
+                                                  <span>Vilnius</span>
+                                                  <span>Galioja iki: 2018.07.01</span>
+                                              </p>
+                                          </header>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
+                                          <span aria-hidden="true">
+                                              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                                  <g fill="none" fill-rule="evenodd" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                                      <path d="M17 1L1 17M1 1l16 16"/>
+                                                  </g>
+                                              </svg>
+                                          </span>
                                         </button>
                                       </div>
                                       <div class="modal-body">
-                                          <div class="row form-group">
-                                            <div class="col">
-                                              <input type="text" class="form-control" placeholder="Vardas">
-                                            </div>
-                                            <div class="col">
-                                              <input type="text" class="form-control" placeholder="Pavardė">
-                                            </div>
-                                          </div>
-                                          <div class="row form-group">
-                                            <div class="col">
-                                              <input type="text" class="form-control" placeholder="El. paštas">
-                                            </div>
-                                            <div class="col">
-                                              <input type="text" class="form-control" placeholder="Tel. numeris">
-                                            </div>
-                                          </div>
-                                          <div class="row form-group">
-                                              <div class="col">
-                                                  <textarea class="form-control" rows="3" placeholder="Žinutė"></textarea>
+                                          <form class="" action="index.html" method="post">
+                                              <div class="row">
+                                                  <div class="col-md-8">
+                                                      <div class="row form-group">
+                                                        <div class="col">
+                                                          <input type="text" class="form-control" placeholder="Vardas">
+                                                        </div>
+                                                        <div class="col">
+                                                          <input type="text" class="form-control" placeholder="Pavardė">
+                                                        </div>
+                                                      </div>
+                                                      <div class="row form-group">
+                                                        <div class="col">
+                                                          <input type="text" class="form-control" placeholder="El. paštas">
+                                                        </div>
+                                                        <div class="col">
+                                                          <input type="text" class="form-control" placeholder="Tel. numeris">
+                                                        </div>
+                                                      </div>
+                                                      <div class="row form-group">
+                                                          <div class="col">
+                                                              <textarea class="form-control" rows="3" placeholder="Žinutė"></textarea>
+                                                          </div>
+                                                      </div>
+                                                  </div>
+                                                  <div class="col-md-4">
+                                                      <div class="row form-group form-group-file">
+                                                          <div class="col">
+                                                              <label for="files">
+                                                                  <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54">
+                                                                      <g fill="none" fill-rule="evenodd" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="5.333" opacity=".05">
+                                                                          <path d="M51 35v10.667A5.333 5.333 0 0 1 45.667 51H8.333A5.333 5.333 0 0 1 3 45.667V35M40.333 16.333L27 3 13.667 16.333M27 3v32"/>
+                                                                      </g>
+                                                                  </svg>
+                                                                  <p>Tempkite ir meskite CV čia arba <a href="#">ieškokite kompiuteryje</a>.</p>
+                                                              </label>
+                                                              <input type="file" id="files" class="form-control" placeholder="Vardas">
+                                                          </div>
+                                                      </div>
+                                                  </div>
                                               </div>
-                                          </div>
-                                          <div class="row">
-
-                                              <button type="button" class="btn btn-primary ml-auto mr-auto" data-dismiss="modal">Siųsti</button>
-
-                                          </div>
+                                              <div class="row">
+                                                  <button type="button" class="btn btn-primary ml-auto mr-auto" data-dismiss="modal">Siųsti</button>
+                                              </div>
+                                           </form>
                                       </div>
                                     </div>
                                   </div>
