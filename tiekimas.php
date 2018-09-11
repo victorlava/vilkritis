413fc828ca09aa4f3cf3119f7b76944e2cde5096
Victor Lava <hello@victorlava.com>
2018-09-11 09:02:41 +0300

    modal

--- tiekimas.php
+++ tiekimas.php
@@ -47,8 +47,86 @@
                               <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                                   <div class="row">
                                       <div class="col-lg-4">
-                                          <div class="section-media section-media-hoverable" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
+                                          <div class="section-media section-media-hoverable" data-toggle="modal" data-target="#aplikuoti1" data-controls-modal="#aplikuoti1" data-backdrop="static" data-keyboard="false" style="background-image:url('img/rectangle-5.jpg'), linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(25, 28, 30, 0.6));">
                                               <h5>Produkto pavadinimas</h5>
+                                          </div>
+                                          <!-- Modal -->
+                                          <div class="modal fade" id="aplikuoti1" tabindex="5" role="dialog" aria-labelledby="aplikuoti1Label1" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+                                            <div class="modal-dialog modal-lg " role="document">
+                                              <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <header class="modal-title">
+                                                        <h4 id="aplikuotiLabel1">Produkto pavadinimas</h4>
+                                                    </header>
+                                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                                            <g fill="none" fill-rule="evenodd" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                                                <path d="M17 1L1 17M1 1l16 16"/>
+                                                            </g>
+                                                        </svg>
+                                                    </span>
+                                                  </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="row">
+                                                        <div class="col-md-5">
+                                                            <div class="modal-media" style="backround-image:url('img/rectangle-5.jpg')">
+
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-7">
+                                                            <p>
+                                                                The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling. Website usability, press releases, online media buys, podcasts, mobile marketing and more.
+                                                            </p>
+
+                                                            <p>The collapse of the online-advertising market in 2001 made marketing on the Internet seem even less compelling.</p>
+
+                                                            <h6>Susiję procesai</h6>
+                                                            <ul>
+                                                                <li>Rotation molding</li>
+                                                                <li>Blow film</li>
+                                                                <li>Blow molding</li>
+                                                            </ul>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button href="#" class="btn btn-secondary accordion-btn collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Susisiekti dėl produkto</button>
+                                                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion" aria-hidden="true">
+                                                     <div class="card-body text-center">
+                                                       <p class="modal-footer-title mt-1 mb-1">
+                                                           <span>+370 5 2691102</span>
+                                                           <span class="dot"></span>
+                                                           <span>info@vilkritis.com</span>
+                                                       </p>
+                                                       <h6 class="mb-2">ARBA</h6>
+                                                       <form class="">
+                                                           <div class="row form-group">
+                                                             <div class="col">
+                                                               <input type="text" class="form-control" placeholder="Vardas">
+                                                             </div>
+                                                             <div class="col">
+                                                               <input type="text" class="form-control" placeholder="El. paštas">
+                                                             </div>
+                                                           </div>
+                                                           <div class="row form-group">
+                                                             <div class="col">
+                                                                 <textarea class="form-control" rows="3" placeholder="Žinutė"></textarea>
+                                                             </div>
+                                                           </div>
+                                                           <div class="row form-group">
+                                                               <div class="col">
+                                                                   <button type="submit" class="btn btn-primary">Siųsti</button>
+                                                               </div>
+                                                           </div>
+                                                       </form>
+                                                     </div>
+                                                   </div>
+                                                </div>
+                                              </div>
+                                            </div>
                                           </div>
                                       </div>
                                       <div class="col-lg-4">
@@ -102,6 +180,19 @@
                 </div>
             </div>
         </section>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+                    $(".section-media").click(function(){
+                        $(".modal").modal({
+                            backdrop: 'static',
+                            keyboard: false
+                        });
+                    });
+
+            })
+        </script>
 
     </main>
 
